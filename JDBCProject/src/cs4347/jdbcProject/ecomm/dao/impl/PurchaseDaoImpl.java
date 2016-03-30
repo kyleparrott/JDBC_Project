2ee0a05ad70e66216c28f878ab23959c8af239f6
kyleparrott <kyle.parrott@gmail.com>
2016-03-30 17:07:38 -0500

    complete purchaseDaoImpl.java

--- JDBCProject/src/cs4347/jdbcProject/ecomm/dao/impl/PurchaseDaoImpl.java
+++ JDBCProject/src/cs4347/jdbcProject/ecomm/dao/impl/PurchaseDaoImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -27,6 +28,7 @@ public class PurchaseDaoImpl implements PurchaseDAO
 		set.next();
 		int id = set.getInt(1);
 		purchase.setId((long) id);
+		connection.commit();
 		return purchase;
 	}
 
@@ -34,22 +36,32 @@ public class PurchaseDaoImpl implements PurchaseDAO
 	public Purchase retrieve(Connection connection, Long id) throws SQLException, DAOException {
 		Statement statement = connection.createStatement();
 		String query = String.format("SELECT * FROM simple_company.Purchase where id = %d", id);
-		ResultSet set = statement.executeQuery(query);
-		Purchase result = new Purchase();
-		result.setId((long) 1234);
-//		result.setCustomerID();
-		return null;
+		ResultSet set = statement.executeQuery(query);		
+		set.next();
+		Purchase result = new Purchase();		
+		result.setCustomerID((long) set.getInt("customerId"));
+		result.setId((long) set.getInt("id"));
+		result.setProductID((long) set.getInt("productId"));
+		result.setPurchaseAmount(set.getDouble("purchaseAmount"));
+		result.setPurchaseDate(set.getDate("purchaseDate"));
+		
+		return result;
 	}
 
 	@Override
 	public int update(Connection connection, Purchase purchase) throws SQLException, DAOException {
-		
-		return 0;
+		Statement statement = connection.createStatement();
+		String query = String.format("UPDATE Purchase SET id = %d, customerId = %d, productId = %d, purchaseDate = '%s', purchaseAmount = %.2f WHERE id = %d", 
+				purchase.getId(), purchase.getCustomerID(), purchase.getProductID(), purchase.getPurchaseDate(), purchase.getPurchaseAmount(), purchase.getId());
+		System.out.println("update query: " + query);
+		int numRows = statement.executeUpdate(query);
+		connection.commit();
+		return numRows;
 	}
 
 	@Override
 	public int delete(Connection connection, Long id) throws SQLException, DAOException {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
@@ -78,15 +90,54 @@ public class PurchaseDaoImpl implements PurchaseDAO
 	@Override
 	public List<Purchase> retrieveForProductID(Connection connection, Long productID)
 			throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		return null;
+
+		Statement statement = connection.createStatement();
+		String query = String.format("SELECT id, purchaseDate, purchaseAmount, customerId, productId FROM simple_company.Purchase where productID = %d", productID);
+		System.out.println("Sql: " + query);
+		ResultSet resSet = statement.executeQuery(query);
+
+		ArrayList<Purchase> result = new ArrayList<Purchase>();
+		int index = 0;
+		while (resSet.next()) {
+			result.add(new Purchase());
+			result.get(index).setId((long) resSet.getInt("id"));
+			result.get(index).setPurchaseAmount(resSet.getFloat("purchaseAmount"));
+			result.get(index).setPurchaseDate(resSet.getDate("purchaseDate"));
+			result.get(index).setProductID((long) resSet.getInt("productId"));
+			result.get(index).setCustomerID((long) resSet.getInt("customerId"));
+			index++;
+		}
+		return result;
 	}
 
 	@Override
 	public PurchaseSummary retrievePurchaseSummary(Connection connection, Long customerID)
 			throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		return null;
+		Statement statement = connection.createStatement();
+		String query = String.format("SELECT id, purchaseDate, purchaseAmount, customerId, productId FROM simple_company.Purchase where customerID = %d", customerID);
+		System.out.println("Sql: " + query);
+		ResultSet resSet = statement.executeQuery(query);
+
+		ArrayList<Float> amounts = new ArrayList<Float>();
+		float total = 0,
+			  min = Float.MAX_VALUE,
+			  max = 0,
+			  size = 0,
+			  current;
+		
+		while (resSet.next()) {
+			current = resSet.getFloat("purchaseAmount");
+			if (current > max) max = current;
+			if (current < min) min = current;
+			total += current;
+			size++;
+		}
+	
+		PurchaseSummary summary = new PurchaseSummary();
+		summary.avgPurchase = total/size;
+		summary.maxPurchase = max;
+		summary.minPurchase = min;
+		return summary;
 	}
 	
 }
