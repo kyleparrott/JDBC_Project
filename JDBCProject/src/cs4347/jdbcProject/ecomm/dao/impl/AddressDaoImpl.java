cefddbfeb7bffb13e202586bd962d8be47426d71
Subayyal Mustafvi <subayyal.mustafvi@gmail.com>
2016-04-03 19:25:32 -0500

    AddressDAOImpl. Still fixing errors.

--- JDBCProject/src/cs4347/jdbcProject/ecomm/dao/impl/AddressDaoImpl.java
+++ JDBCProject/src/cs4347/jdbcProject/ecomm/dao/impl/AddressDaoImpl.java
@@ -13,7 +13,7 @@ public class AddressDaoImpl implements AddressDAO
 	public Address create(Connection connection, Address address, Long customerID) throws SQLException, DAOException {
 		// TODO Auto-generated method stub
 		Statement state = connection.createStatement();
-		String sql = String.format("INSERT INTO Address VALUES ('%s', '%s', '%s', '%s', '%s', %d);", address.getAddress1(), address.getAddress2(),
+		String sql = String.format("INSERT INTO address values ('%s', '%s', '%s', '%s', '%s', %d);", address.getAddress1(), address.getAddress2(),
 				address.getCity(), address.getState(), address.getZipcode(), customerID);
 		state.executeUpdate(sql);
 		return address;
@@ -21,13 +21,33 @@ public class AddressDaoImpl implements AddressDAO
 
 	@Override
 	public Address retrieveForCustomerID(Connection connection, Long customerID) throws SQLException, DAOException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Statement statement = connection.createStatement();
+		String query = String.format("SELECT address1, address2, city, state, zipcode FROM simple_company.address where customerId = %d", customerID);
+		ResultSet resSet = statement.executeQuery(query);
+		
+		if (!resSet.next()){
+			return null;
+		}
+		
+		Address result = new Address();
+		result.setAddress1(resSet.getString("address1"));
+		result.setAddress2(resSet.getString("address2"));
+		result.setCity(resSet.getString("city"));
+		result.setState(resSet.getString("state"));
+		result.setZipcode(resSet.getString("zipcode"));
+		return result;
 	}
 
 	@Override
 	public void deleteForCustomerID(Connection connection, Long customerID) throws SQLException, DAOException {
 		// TODO Auto-generated method stub
+		
+		Statement statement = connection.createStatement();
+		String query = String.format("DELETE FROM simple_company.address where customerId = %d", customerID);
+		int numRows = statement.executeUpdate(query);
+		return;
+		
 		
 	}
 }
